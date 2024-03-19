bb7bbc6b5f1db4265f9a2b2c34bc26920f1fa663
josuebaez61@gmail.com <josuebaez61@gmail.com>
2024-03-19 10:41:42 -0300

    CHANGES

--- dist/index.js
+++ dist/index.js
@@ -32,9 +32,54 @@ var Roles;
     // Usuario = 'usuario'
     Roles["Usuario"] = "user";
 })(Roles || (Roles = {}));
+class Usuario {
+    constructor(nombre, apellido, edad, role) {
+        // constructor(nombre: string, edad: number, role: Roles = Roles.Usuario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.role = role || Roles.Usuario;
+    }
+    get nombreCompleto() {
+        return this.nombre + " " + this.apellido;
+    }
+    set nombreCompleto(val) {
+        // Peter Parker
+        const nombreDivido = val.split(" ");
+        this.nombre = nombreDivido[0]; // Peter
+        this.apellido = nombreDivido[1]; // Parker
+    }
+    iniciarSesion() {
+        console.log(`Sesion iniciada, su edad es ${this.edad}`);
+    }
+    cerrarSesion() { }
+    inscribirseACurso() { }
+}
+const spiderman = new Usuario("Peter", "Parker", 15, Roles.Administrador);
+const superman = new Usuario("Clark", "Ken", 35);
+console.log(spiderman.nombreCompleto);
+spiderman.nombreCompleto = "Peter Parker";
+// spiderman.iniciarSesion();
+// superman.iniciarSesion();
 let usuarios = [
-    { nombre: "Naruto", edad: 13, role: Roles.Administrador },
-    // { nombre: "Sasuke", edad: 13 },
+    spiderman,
+    superman,
+    // {
+    //   nombre: "Naruto",
+    //   edad: 13,
+    //   role: Roles.Administrador,
+    //   iniciarSesion: () => {
+    //     // Alguna logica
+    //   },
+    // },
+    // {
+    //   nombre: "Sasuke",
+    //   edad: 13,
+    //   role: Roles.Usuario,
+    //   iniciarSesion: () => {
+    //     // Alguna logica repetida
+    //   },
+    // },
     // { nombre: "Kakashi", edad: 20 },
 ]; // { nombre: string; }[];
 function validadIsAdmin(role) {
@@ -44,3 +89,35 @@ const user = {
     role: "user",
 };
 validadIsAdmin(user.role);
+let nombreEdad = ["Sakura", 13];
+let objeto = {};
+const myUsersPagination = {
+    data: [spiderman, superman],
+    pagina: 1,
+    total: 10,
+    tieneAnterior: false,
+    tieneSiguiente: true,
+};
+const productos = {
+    data: [
+        {
+            nombre: "PC GAMER",
+            precio: 9999,
+        },
+        {
+            nombre: "Cafetera",
+            precio: 1000,
+        },
+    ],
+    pagina: 1,
+    total: 10,
+    tieneAnterior: false,
+    tieneSiguiente: true,
+};
+// interface PaginacionProductos {
+//   data: Producto[]
+//   agina: number;
+//   total: number;
+//   tieneSiguiente: boolean;
+//   tieneAnterior: boolean;
+// }
--- index.ts
+++ index.ts
@@ -33,23 +33,82 @@ const RolesObj = {
   Administrador: "admin",
   Usuario: "admin",
 };
-
-interface Usuario {
-  nombre: string;
-  edad: number;
-  role: Roles;
-}
-
 enum Roles {
   Administrador = "admin",
   // Usuario = 'Usuario'
   // Usuario = 'usuario'
   Usuario = "user",
 }
+interface IUsuario {
+  nombre: string;
+  edad: number;
+  role: Roles;
+  iniciarSesion: () => void;
+}
+
+class Usuario {
+  nombre: string;
+  apellido: string;
+  private edad: number;
+  role: Roles;
+
+  constructor(nombre: string, apellido: string, edad: number, role?: Roles) {
+    // constructor(nombre: string, edad: number, role: Roles = Roles.Usuario) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.role = role || Roles.Usuario;
+  }
+
+  get nombreCompleto() {
+    return this.nombre + " " + this.apellido;
+  }
+
+  set nombreCompleto(val: string) {
+    // Peter Parker
+    const nombreDivido = val.split(" ");
+    this.nombre = nombreDivido[0]; // Peter
+    this.apellido = nombreDivido[1]; // Parker
+  }
+
+  iniciarSesion() {
+    console.log(`Sesion iniciada, su edad es ${this.edad}`);
+  }
+
+  cerrarSesion() {}
+
+  inscribirseACurso() {}
+}
+
+const spiderman = new Usuario("Peter", "Parker", 15, Roles.Administrador);
+const superman = new Usuario("Clark", "Ken", 35);
+
+console.log(spiderman.nombreCompleto);
+
+spiderman.nombreCompleto = "Peter Parker";
+
+// spiderman.iniciarSesion();
+// superman.iniciarSesion();
 
 let usuarios: Usuario[] = [
-  { nombre: "Naruto", edad: 13, role: Roles.Administrador },
-  // { nombre: "Sasuke", edad: 13 },
+  spiderman,
+  superman,
+  // {
+  //   nombre: "Naruto",
+  //   edad: 13,
+  //   role: Roles.Administrador,
+  //   iniciarSesion: () => {
+  //     // Alguna logica
+  //   },
+  // },
+  // {
+  //   nombre: "Sasuke",
+  //   edad: 13,
+  //   role: Roles.Usuario,
+  //   iniciarSesion: () => {
+  //     // Alguna logica repetida
+  //   },
+  // },
   // { nombre: "Kakashi", edad: 20 },
 ]; // { nombre: string; }[];
 
@@ -62,3 +121,67 @@ const user = {
 };
 
 validadIsAdmin(user.role);
+
+let nombreEdad: [string, number] = ["Sakura", 13];
+
+let objeto: object = {};
+
+// Object.
+
+// Object.assign()
+// Object.freeze()
+// Array.isArray()
+
+// interface PaginacionUsuarios {
+//   data: Usuario[];
+//   pagina: number;
+//   total: number;
+//   tieneSiguiente: boolean;
+//   tieneAnterior: boolean;
+// }
+
+interface Paginacion<TipoData> {
+  data: TipoData[];
+  pagina: number;
+  total: number;
+  tieneSiguiente: boolean;
+  tieneAnterior: boolean;
+}
+
+const myUsersPagination: Paginacion<Usuario> = {
+  data: [spiderman, superman],
+  pagina: 1,
+  total: 10,
+  tieneAnterior: false,
+  tieneSiguiente: true,
+};
+
+interface Producto {
+  precio: number;
+  nombre: string;
+}
+
+const productos: Paginacion<Producto> = {
+  data: [
+    {
+      nombre: "PC GAMER",
+      precio: 9999,
+    },
+    {
+      nombre: "Cafetera",
+      precio: 1000,
+    },
+  ],
+  pagina: 1,
+  total: 10,
+  tieneAnterior: false,
+  tieneSiguiente: true,
+};
+
+// interface PaginacionProductos {
+//   data: Producto[]
+//   agina: number;
+//   total: number;
+//   tieneSiguiente: boolean;
+//   tieneAnterior: boolean;
+// }
